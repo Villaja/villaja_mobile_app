e442b90810413ae2b08d29927fccc9f5934c2857
daniel-fasiku <daniel.fasiku@stu.cu.edu.ng>
2024-03-08 19:05:30 +0100

    login/signup minor adjustments

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -2,7 +2,7 @@ import { View, StyleSheet, TextInput, Text, TouchableOpacity, Image, Alert, Scro
 import CheckBox from '@react-native-community/checkbox';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
-import { defaultStyles} from '../../constants/Styles'
+import { defaultStyles } from '../../constants/Styles'
 import Colors from '../../constants/Colors';
 import { useState } from 'react';
 import { AntDesign, FontAwesome5 } from '@expo/vector-icons';
@@ -16,7 +16,7 @@ import { useAuth } from '../../context/AuthContext';
 const Page = () => {
   useWarmUpBrowser();
   const [toggleCheckBox, setToggleCheckBox] = useState<boolean>(false)
-  const [passwordVisible,setPasswordVisisble] = useState<boolean>(true)
+  const [passwordVisible, setPasswordVisisble] = useState<boolean>(true)
 
   const { login, isLoading, error, user } = useAuth();
   const [email, setEmail] = useState('');
@@ -33,84 +33,84 @@ const Page = () => {
   };
 
   useEffect(() => {
-    if(user){
+    if (user) {
       router.replace('/')
       console.log("successs")
     }
-    
+
   })
 
   return (
-    <ScrollView style={{flex:1,backgroundColor:'#fff'}}>
-      
-    <View style={styles.container}>
-      <Text style={styles.headerText}>Welcome Back</Text>
-      <Text style={styles.headerInfo}>You Are Welcome Back To Villaja. Please Fill In Your Details</Text>
+    <ScrollView style={{ flex: 1, backgroundColor: '#fff' }}>
 
-      <View style={{marginTop:30,gap:12}}>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Email</Text>
-          <TextInput style={defaultStyles.inputField}  returnKeyType="done" placeholder='Enter Your Email' value={email}
-            onChangeText={(text) => setEmail(text)} placeholderTextColor={'rgba(0,0,0,0.20)'} />
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Welcome Back</Text>
+        <Text style={styles.headerInfo}>You Are Welcome Back To Villaja. Please Fill In Your Details</Text>
+
+        <View style={{ marginTop: 30, gap: 12 }}>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Email</Text>
+            <TextInput style={defaultStyles.inputField} returnKeyType="done" placeholder='Enter Your Email' value={email}
+              onChangeText={(text) => setEmail(text)} placeholderTextColor={'rgba(0,0,0,0.20)'} />
+          </View>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Password</Text>
+            <TextInput style={defaultStyles.inputField} secureTextEntry={passwordVisible} value={password}
+              onChangeText={(text) => setPassword(text)} placeholder='Enter Your Password' returnKeyType="done" placeholderTextColor={'rgba(0,0,0,0.20)'} />
+            {error && <Text style={{ color: 'red', marginTop: 10, marginBottom: 10 }}>{error}</Text>}
+            <TouchableOpacity style={{ position: 'absolute', right: 1, top: 33, width: 40, height: 30, justifyContent: "center", alignItems: "center" }} onPress={() => setPasswordVisisble(oldvalue => !oldvalue)}>
+              <AntDesign name="eye" size={18} color={Colors.grey} />
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Password</Text>
-          <TextInput style={defaultStyles.inputField} secureTextEntry={passwordVisible} value={password}
-            onChangeText={(text) => setPassword(text)} placeholder='Enter Your Password' returnKeyType="done" placeholderTextColor={'rgba(0,0,0,0.20)'} />
-             {error && <Text style={{ color: 'red', marginTop: 10, marginBottom: 10}}>{error}</Text>}
-          <TouchableOpacity style={{position:'absolute',right:6,top:36}} onPress={() => setPasswordVisisble(oldvalue => !oldvalue)}> 
-            <AntDesign name="eye" size={18} color={Colors.grey}  />
+
+        <View style={{ marginVertical: 6, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <BouncyCheckbox
+            size={17}
+            fillColor={Colors.primary}
+            unfillColor="#FFFFFF"
+            text="Remember me"
+            iconStyle={{ borderColor: Colors.primary }}
+            innerIconStyle={{ borderWidth: 2 }}
+            textStyle={{ fontFamily: "roboto-condensed", fontSize: 13 }}
+            onPress={(isChecked: boolean) => { setToggleCheckBox(isChecked) }}
+          />
+
+          <Link href={'/(modals)/forgotPassword'}>
+            <Text style={{ color: Colors.grey, textDecorationLine: 'underline', fontSize: 10, fontFamily: 'roboto-condensed' }}>Forgot Your Password?</Text>
+          </Link>
+        </View>
+
+        <View style={{ marginTop: 25, gap: 13 }}>
+          <TouchableOpacity style={defaultStyles.btn} onPress={handleLogin}>
+            <Text style={defaultStyles.btnText}>{isLoading ? 'Loading...' : 'Login'}</Text>
+          </TouchableOpacity>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 13, color: "#00000050", fontWeight: "500" }}>Don't have and account?</Text>
+            <Link href='/(modals)/register'> <Text style={{ fontFamily: 'roboto-condensed-sb', color: Colors.primary }}>Sign Up</Text></Link>
+          </View>
+        </View>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 2, marginVertical: 26 }}>
+          <View style={{ borderBottomColor: Colors.grey, borderBottomWidth: StyleSheet.hairlineWidth, flexGrow: 1 }}></View>
+          <Text style={{ fontFamily: 'roboto-condensed', fontSize: 13, color: Colors.grey }}>OR</Text>
+          <View style={{ borderBottomColor: Colors.grey, borderBottomWidth: StyleSheet.hairlineWidth, flexGrow: 1 }}></View>
+        </View>
+
+        <View style={{ gap: 12 }}>
+          <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
+            <Image source={require('../../assets/images/google.png')} style={{ width: 15, height: 15 }} />
+            <Text style={[defaultStyles.btnText, { color: '#00000080', fontFamily: 'roboto-condensed', fontWeight: "700", fontSize: 13 }]}>Login With Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
+            <Image source={require('../../assets/images/facebook.png')} style={{ width: 15, height: 15 }} />
+            <Text style={[defaultStyles.btnText, { color: '#00000080', fontFamily: 'roboto-condensed', fontWeight: "700", fontSize: 13 }]}>Login With Facebook</Text>
           </TouchableOpacity>
         </View>
+
+
+
       </View>
-
-      <View style={{marginVertical:6,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-        <BouncyCheckbox
-          size={25}
-          fillColor={Colors.primary}
-          unfillColor="#FFFFFF"
-          text="Remember me"
-          iconStyle={{ borderColor:Colors.primary }}
-          innerIconStyle={{ borderWidth: 2 }}
-          textStyle={{ fontFamily: "roboto-condensed",fontSize:13 }}
-          onPress={(isChecked: boolean) => {setToggleCheckBox(isChecked)}}
-        />
-
-        <Link href={'/(modals)/forgotPassword'}> 
-        <Text style={{color:Colors.grey,textDecorationLine:'underline',fontSize:10,fontFamily:'roboto-condensed'}}>Forgot Your Password?</Text>
-        </Link>
-      </View>
-
-      <View style={{marginTop:15,gap:13}}>
-        <TouchableOpacity style={defaultStyles.btn} onPress={handleLogin}>
-          <Text style={defaultStyles.btnText}>{isLoading ? 'Loading...' : 'Login'}</Text>
-        </TouchableOpacity>
-        <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
-          <Text style={{fontFamily:'roboto-condensed'}}>Dont have and account?</Text>
-          <Link href='/(modals)/register'> <Text style={{fontFamily:'roboto-condensed-sb',color:Colors.primary}}>Sign Up</Text></Link>
-        </View>
-      </View>
-
-      <View style={{flexDirection:'row',alignItems:'center',gap:2,marginVertical:26}}>
-        <View style={{borderBottomColor:Colors.grey,borderBottomWidth:StyleSheet.hairlineWidth,flexGrow:1}}></View>
-        <Text style={{fontFamily:'roboto-condensed',fontSize:13,color:Colors.grey}}>OR</Text>
-        <View style={{borderBottomColor:Colors.grey,borderBottomWidth:StyleSheet.hairlineWidth,flexGrow:1}}></View>
-      </View>
-
-      <View style={{gap:12}}>
-        <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
-          <AntDesign name="google" size={15} color={Colors.primary} />
-          <Text style={[defaultStyles.btnText,{color:'#000',fontFamily:'roboto-condensed'}]}>Login With Google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
-          <FontAwesome5 name="facebook" size={15} color={Colors.primary} />
-          <Text style={[defaultStyles.btnText,{color:'#000',fontFamily:'roboto-condensed'}]}>Login With Facebook</Text>
-        </TouchableOpacity>
-      </View>
-
-      
-      
-    </View>
     </ScrollView>
 
   );
@@ -122,21 +122,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingVertical:6,
-    paddingHorizontal:20
+    paddingVertical: 35,
+    paddingHorizontal: 20
   },
-  headerText:{
-    fontFamily:'roboto-condensed-sb-i',
-    fontSize:35,
-    color:Colors.primary,
-    marginBottom:4
+  headerText: {
+    fontFamily: 'roboto-condensed-sb-i',
+    fontSize: 35,
+    color: Colors.primary,
+    marginBottom: 4
   },
-  headerInfo:{
-    color:Colors.grey,
-    fontFamily:'roboto-condensed',
-    fontSize:16
+  headerInfo: {
+    color: Colors.grey,
+    fontFamily: 'roboto-condensed',
+    fontSize: 15
   }
 
 
-  
+
 });
--- app/(modals)/register.tsx
+++ app/(modals)/register.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, TextInput, Text, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import { View, StyleSheet, TextInput, Text, TouchableOpacity, Alert, ScrollView, Image } from 'react-native';
 import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
 import { defaultStyles } from '../../constants/Styles';
 import { AntDesign, FontAwesome5 } from '@expo/vector-icons';
@@ -23,8 +23,8 @@ const Page = () => {
   const [password, setPassword] = useState('');
 
 
-  const [passwordVisible,setPasswordVisisble] = useState<boolean>(true)
-  const [confirmPasswordVisible,setConfirmPasswordVisisble] = useState<boolean>(true)
+  const [passwordVisible, setPasswordVisisble] = useState<boolean>(true)
+  const [confirmPasswordVisible, setConfirmPasswordVisisble] = useState<boolean>(true)
 
   const handleRegister = async () => {
     try {
@@ -39,7 +39,7 @@ const Page = () => {
   };
 
   useEffect(() => {
-    if(user){
+    if (user) {
       router.replace('/(tabs)')
     }
   })
@@ -48,95 +48,92 @@ const Page = () => {
 
   return (
     <ScrollView>
-    <View style={styles.container}>
-      <Text style={styles.headerText}>Lets Get Started</Text>
-      <Text style={styles.headerInfo}>You Are Welcome Back To Villaja. Please Fill In Your Details To Create Your Account</Text>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Lets Get Started</Text>
+        <Text style={styles.headerInfo}>You Are Welcome Back To Villaja. Please Fill In Your Details To Create Your Account</Text>
 
-      <View style={{marginTop:30,gap:12}}>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Firstname</Text>
-          <TextInput style={defaultStyles.inputField} value={firstname}
-            onChangeText={(text) => setFirstname(text)} placeholder='Enter Your Firstname' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+        <View style={{ marginTop: 30, gap: 12 }}>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>First Name</Text>
+            <TextInput style={defaultStyles.inputField} value={firstname}
+              onChangeText={(text) => setFirstname(text)} placeholder='Enter Your Firstname' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+          </View>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Last Name</Text>
+            <TextInput style={defaultStyles.inputField} value={lastname}
+              onChangeText={(text) => setLastname(text)} placeholder='Enter Your Last' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+          </View>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Email</Text>
+            <TextInput style={defaultStyles.inputField} value={email}
+              onChangeText={(text) => setEmail(text)} returnKeyType="done" placeholder='Enter Your Email' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+          </View>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Phone Number</Text>
+            <TextInput style={defaultStyles.inputField} value={phoneNumber}
+              onChangeText={(text) => setPhonenumber(text)} returnKeyType="done" placeholder='Enter Your Email' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+          </View>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Password</Text>
+            <TextInput style={defaultStyles.inputField} secureTextEntry={passwordVisible} placeholder='Enter Your Password' value={password} returnKeyType="done"
+              onChangeText={(text) => setPassword(text)} placeholderTextColor={'rgba(0,0,0,0.20)'} />
+            <TouchableOpacity style={{ position: 'absolute', right: 6, top: 36 }} onPress={() => setPasswordVisisble(oldvalue => !oldvalue)}>
+              <AntDesign name="eye" size={18} color={Colors.grey} />
+            </TouchableOpacity>
+          </View>
+          <View style={{ gap: 4 }}>
+            <Text style={{ fontFamily: 'roboto-condensed', fontSize: 15, color: 'rgba(0,0,0,0.70)' }}>Confirm Password</Text>
+            <TextInput style={defaultStyles.inputField} returnKeyType="done" secureTextEntry={confirmPasswordVisible} placeholder='Confirm Your Password' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+            <TouchableOpacity style={{ position: 'absolute', right: 6, top: 36 }} onPress={() => setConfirmPasswordVisisble(oldvalue => !oldvalue)}>
+              <AntDesign name="eye" size={18} color={Colors.grey} />
+            </TouchableOpacity>
+          </View>
+          {error && <Text style={{ color: 'red', marginTop: 10, marginBottom: 10 }}>{error}</Text>}
         </View>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Lastname</Text>
-          <TextInput style={defaultStyles.inputField} value={lastname}
-            onChangeText={(text) => setLastname(text)} placeholder='Enter Your Last' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+
+        <View style={{ marginVertical: 6, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <BouncyCheckbox
+            size={18}
+            fillColor={Colors.primary}
+            unfillColor="#FFFFFF"
+            text="I Agree with the Terms and Conditions"
+            iconStyle={{ borderColor: Colors.primary }}
+            innerIconStyle={{ borderWidth: 2 }}
+            textStyle={{ fontFamily: "roboto-condensed", fontSize: 9, }}
+            onPress={(isChecked: boolean) => { setToggleCheckBox(isChecked) }}
+          />
         </View>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Email</Text>
-          <TextInput style={defaultStyles.inputField} value={email}
-            onChangeText={(text) => setEmail(text)} returnKeyType="done" placeholder='Enter Your Email' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+
+        <View style={{ marginTop: 15, gap: 13 }}>
+          <TouchableOpacity style={defaultStyles.btn} onPress={handleRegister}>
+            <Text style={defaultStyles.btnText}>{isLoading ? 'Loading...' : 'Contnue'}</Text>
+          </TouchableOpacity>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontFamily: 'roboto-condensed', color: "#00000050" }}>Already have and account?</Text>
+            <Link href='/(modals)/login'> <Text style={{ fontFamily: 'roboto-condensed-sb', color: Colors.primary }}>Sign In</Text></Link>
+          </View>
         </View>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Phone Number</Text>
-          <TextInput style={defaultStyles.inputField} value={phoneNumber}
-            onChangeText={(text) => setPhonenumber(text)} returnKeyType="done" placeholder='Enter Your Email' placeholderTextColor={'rgba(0,0,0,0.20)'} />
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 2, marginVertical: 16 }}>
+          <View style={{ borderBottomColor: Colors.grey, borderBottomWidth: StyleSheet.hairlineWidth, flexGrow: 1 }}></View>
+          <Text style={{ fontFamily: 'roboto-condensed', fontSize: 13, color: Colors.grey }}>OR</Text>
+          <View style={{ borderBottomColor: Colors.grey, borderBottomWidth: StyleSheet.hairlineWidth, flexGrow: 1 }}></View>
         </View>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Password</Text>
-          <TextInput style={defaultStyles.inputField} secureTextEntry={passwordVisible} placeholder='Enter Your Password' value={password} returnKeyType="done"
-            onChangeText={(text) => setPassword(text)} placeholderTextColor={'rgba(0,0,0,0.20)'} />
-          <TouchableOpacity style={{position:'absolute',right:6,top:36}} onPress={() => setPasswordVisisble(oldvalue => !oldvalue)}> 
-            <AntDesign name="eye" size={18} color={Colors.grey}  />
+
+        <View style={{ gap: 12 }}>
+          <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
+            <Image source={require('../../assets/images/google.png')} style={{ width: 15, height: 15 }} />
+            <Text style={[defaultStyles.btnText, { color: '#00000080', fontFamily: 'roboto-condensed', fontWeight: "700", fontSize: 13 }]}>Login With Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
+            <Image source={require('../../assets/images/facebook.png')} style={{ width: 15, height: 15 }} />
+            <Text style={[defaultStyles.btnText, { color: '#00000080', fontFamily: 'roboto-condensed', fontWeight: "700", fontSize: 13 }]}>Login With Facebook</Text>
           </TouchableOpacity>
         </View>
-        <View style={{gap:4}}>
-          <Text style={{fontFamily:'roboto-condensed',fontSize:15,color:'rgba(0,0,0,0.70)'}}>Confirm Password</Text>
-          <TextInput style={defaultStyles.inputField} returnKeyType="done" secureTextEntry={confirmPasswordVisible} placeholder='Confirm Your Password' placeholderTextColor={'rgba(0,0,0,0.20)'} />
-          <TouchableOpacity style={{position:'absolute',right:6,top:36}} onPress={() => setConfirmPasswordVisisble(oldvalue => !oldvalue)}> 
-            <AntDesign name="eye" size={18} color={Colors.grey}  />
-          </TouchableOpacity>
-        </View>
-        {error && <Text style={{ color: 'red', marginTop: 10, marginBottom: 10}}>{error}</Text>}
+
+
+
       </View>
-
-      <View style={{marginVertical:6,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-        <BouncyCheckbox
-          size={25}
-          fillColor={Colors.primary}
-          unfillColor="#FFFFFF"
-          text="I Agree with the Terms and Conditions"
-          
-          iconStyle={{ borderColor:Colors.primary }}
-          innerIconStyle={{ borderWidth: 2 }}
-          textStyle={{ fontFamily: "roboto-condensed",fontSize:9,}}
-          onPress={(isChecked: boolean) => {setToggleCheckBox(isChecked)}}
-        />
-
-        <Text style={{color:Colors.grey,textDecorationLine:'underline',fontSize:10,fontFamily:'roboto-condensed'}}>Forgot Your Password?</Text>
-      </View>
-
-      <View style={{marginTop:15,gap:13}}>
-        <TouchableOpacity style={defaultStyles.btn} onPress={handleRegister}>
-          <Text style={defaultStyles.btnText}>{isLoading ? 'Loading...' : 'Contnue'}</Text>
-        </TouchableOpacity>
-        <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
-          <Text style={{fontFamily:'roboto-condensed'}}>Already have and account?</Text>
-          <Link href='/(modals)/login'> <Text style={{fontFamily:'roboto-condensed-sb',color:Colors.primary}}>Sign In</Text></Link>
-        </View>
-      </View>
-
-      <View style={{flexDirection:'row',alignItems:'center',gap:2,marginVertical:16}}>
-        <View style={{borderBottomColor:Colors.grey,borderBottomWidth:StyleSheet.hairlineWidth,flexGrow:1}}></View>
-        <Text style={{fontFamily:'roboto-condensed',fontSize:13,color:Colors.grey}}>OR</Text>
-        <View style={{borderBottomColor:Colors.grey,borderBottomWidth:StyleSheet.hairlineWidth,flexGrow:1}}></View>
-      </View>
-
-      <View style={{gap:12}}>
-        <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
-          <AntDesign name="google" size={15} color={Colors.primary} />
-          <Text style={[defaultStyles.btnText,{color:'#000',fontFamily:'roboto-condensed'}]}>Login With Google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={defaultStyles.ThirdPartyBtn}>
-          <FontAwesome5 name="facebook" size={15} color={Colors.primary} />
-          <Text style={[defaultStyles.btnText,{color:'#000',fontFamily:'roboto-condensed'}]}>Login With Facebook</Text>
-        </TouchableOpacity>
-      </View>
-
-      
-      
-    </View>
     </ScrollView>
   );
 };
@@ -147,21 +144,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingVertical:6,
-    paddingHorizontal:20
+    paddingVertical: 4,
+    paddingHorizontal: 20
   },
-  headerText:{
-    fontFamily:'roboto-condensed-sb-i',
-    fontSize:35,
-    color:Colors.primary,
-    marginBottom:4
+  headerText: {
+    fontFamily: 'roboto-condensed-sb-i',
+    fontSize: 35,
+    color: Colors.primary,
+    marginBottom: 4
   },
-  headerInfo:{
-    color:Colors.grey,
-    fontFamily:'roboto-condensed',
-    fontSize:16
+  headerInfo: {
+    color: Colors.grey,
+    fontFamily: 'roboto-condensed',
+    fontSize: 15
   }
 
 
-  
+
 });
