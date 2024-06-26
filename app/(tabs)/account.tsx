e993f0471f9bf233d80b1a0ebc810e68bd59c9ba
Fasiku Daniel <daniel.fasiku@stu.cu.edu.ng>
2024-06-26 11:10:13 +0100

    corrections

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,74 +1,60 @@
-import { View, Text, Button, SafeAreaView, Pressable,StyleSheet, Platform } from 'react-native'
+import { View, Text, Button, SafeAreaView, Pressable, StyleSheet, Platform, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from 'expo-router'
-
-import React, { useState } from 'react'
-import { Link, useRouter } from 'expo-router'
+import { useRouter } from 'expo-router';
+import React, { useState, useEffect } from 'react';
 import Colors from '../../constants/Colors';
 import Messages from '../userNotificationsTabs/Messages';
 import Notifications from '../userNotificationsTabs/Notifications';
-
-const handleLogout = async () => {
-
-  await AsyncStorage.removeItem('token');
-  router.replace('/');
-};
+import { useAuth } from '../../context/AuthContext';
 
 const account = () => {
-  const router = useRouter()
-  const [activeTab,setActiveTab] = useState<string>("messages")
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState<string>('messages');
+  const { user } = useAuth();
 
-  
+  useEffect(() => {
+    if (!user) {
+      Alert.alert('Hello', 'Please login to be able to send messages');
+      router.push('/(modals)/login');
+    }
+  }, [user]);
+
   return (
     <SafeAreaView style={styles.container}>
+      <View style={Platform.OS === 'android' ? { flex: 1, paddingTop: 40 } : { flex: 1 }}>
+        <View style={styles.tabs}>
+          <Pressable onPress={() => setActiveTab('messages')}>
+            <Text style={[styles.tabText, activeTab === 'messages' ? { color: Colors.primary } : { color: 'rgba(0,0,0,0.50)' }]}>Messages</Text>
+          </Pressable>
+          <Pressable onPress={() => setActiveTab('notifications')}>
+            <Text style={[styles.tabText, activeTab === 'notifications' ? { color: Colors.primary } : { color: 'rgba(0,0,0,0.50)' }]}>Notifications</Text>
+          </Pressable>
+        </View>
 
-    <View style={Platform.OS === "android" ? {flex:1,paddingTop:40}:{flex:1}} >
-      <View style={styles.tabs}>
-        <Pressable onPress={() => setActiveTab('messages')} >
-          <Text style={[styles.tabText,activeTab === "messages" ? {color:Colors.primary}:{color:'rgba(0,0,0,0.50)'}]}>Messages</Text>
-        </Pressable>
-        <Pressable onPress={() => setActiveTab('notifications')} >
-          <Text style={[styles.tabText,activeTab === "notifications" ? {color:Colors.primary}:{color:'rgba(0,0,0,0.50)'}]}>Notifications</Text>
-        </Pressable>
+        {user && (activeTab === 'messages' ? <Messages /> : <Notifications />)}
       </View>
-
-      {
-        activeTab === "messages" ? 
-        <Messages/> 
-        :
-        <Notifications/>
-      }
-    </View>
     </SafeAreaView>
-    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    //   <Button title='Login' onPress={() => router.push('/(modals)/landing')} />
-
-    //   <Button title='Logout' onPress={handleLogout} />
-    //   <Button title='more screen' onPress={() => router.push('/(modals)/firstLoadingScreen')} />
-    //   <Button title='seller dashboard' onPress={() => router.push('/(tabs2)/sellerDashboard')} />
-
-    // </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
 
-    flex:1,
-    backgroundColor:'#fff'
+    flex: 1,
+    backgroundColor: '#fff'
   },
-  tabs:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center',
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     paddingHorizontal: 60,
-    paddingVertical:16,
-    borderBottomWidth:1,
-    borderBottomColor:'rgba(0,0,0,0.05)'
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0,0,0,0.05)'
   },
-  tabText:{
-    fontFamily:'roboto-condensed-m',
-    fontSize:15,
+  tabText: {
+    fontFamily: 'roboto-condensed-m',
+    fontSize: 15,
     lineHeight: 20,
     letterSpacing: -0.24
   }
